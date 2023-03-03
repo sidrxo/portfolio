479c7d238e6da5441b9d57c455ddf56e95d37613
sidrxo <sidrxo@gmail.com>
2023-03-03 17:19:45 +0000

    lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,12 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 } 
 
-document.addEventListener("DOMContentLoaded", function() {
-  const projectTiles = document.querySelectorAll(".project-tile");
-  projectTiles.forEach(tile => {
-    tile.addEventListener("click", function() {
-      const pageUrl = this.getAttribute("data-page-url");
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", pageUrl, true);
-      xhr.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const newContent = this.responseText;
-          const bioheader = document.getElementById("bioheader");
-          bioheader.innerHTML = newContent;
-        }
-      };
-      xhr.send();
+$(document).ready(function() {
+  $('.wide').click(function() {
+    var url = $(this).data('url');
+    $('#bioheader').load(url, function() {
+      $('#bioheader').addClass('slide-in-right');
     });
+    $('.doubletile').addClass('slide-out-left');
   });
 });
