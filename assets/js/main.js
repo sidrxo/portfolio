fa8cfea6a481a49d907c063f8681faf46041c78d
sidrxo <sidrxo@gmail.com>
2023-03-03 16:23:17 +0000

    ajaxattempt2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,25 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 } 
 
-function loadContent(url, target) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      var response = xhr.responseText;
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(response, 'text/html');
-      var content = doc.querySelector(target).innerHTML;
-      document.querySelector('#' + target).innerHTML = content;
-    }
-    else {
-      console.log('Request failed.  Returned status of ' + xhr.status);
-    }
-  };
-  xhr.send();
+$(document).ready(function() {
+  // Add click event listeners to project tiles
+  $('.project-tile').on('click', function(e) {
+      e.preventDefault();
+      var url = $(this).data('url');
+      loadProject(url);
+  });
+});
+
+function loadProject(url) {
+  // Send Ajax request to load project content
+  $.ajax({
+      url: url,
+      success: function(data) {
+          // Insert project content into container element
+          $('#project-container').html(data);
+      },
+      error: function() {
+          alert('Error loading project content');
+      }
+  });
 }
