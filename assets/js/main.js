9a5baec294e196f13000b695ba57ee85a809dea9
sidrxo <sidrxo@gmail.com>
2023-03-03 16:43:26 +0000

    asdfasdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,9 @@ function loadProjectPage(pageUrl) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          var oldContent = document.getElementById("projectContent");
-          oldContent.style.display = "none"; // hide the old content
-          oldContent.innerHTML = this.responseText;
-          oldContent.style.display = "block"; // show the new content
+          document.getElementById("projectContent").innerHTML = this.responseText;
       }
   };
   xhr.open("GET", pageUrl, true);
   xhr.send();
-}
-function loadNewPage(pageUrl) {
-  window.location.replace(pageUrl);
 }
