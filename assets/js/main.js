53596bda415cd01b6f11536cbe06befd818fef24
sidrxo <sidrxo@gmail.com>
2023-03-03 16:20:15 +0000

    ajax attempt1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,20 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 } 
 
+function loadContent(url, target) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var response = xhr.responseText;
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(response, 'text/html');
+      var content = doc.querySelector(target).innerHTML;
+      document.querySelector('#' + target).innerHTML = content;
+    }
+    else {
+      console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();
+}
