f62729d6aca6ea1c7bd8c6435ffa43caa49832d7
sidrxo <sidrxo@gmail.com>
2023-03-04 01:33:10 +0000

    rr

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ function loadProjectPage(url) {
       $('#projectContent').html(data);
       // Fade in the #projectContent element
       $('#projectContent').fadeIn('slow');
+      return false
     });
   });
-  return false
 }
