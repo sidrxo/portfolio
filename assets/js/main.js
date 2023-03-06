4e2ad96d32b91bd33145c0b40db9bd212ae7185a
sidrxo <sidrxo@gmail.com>
2023-03-06 15:08:23 +0000

    aligining project page

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ function myFunction() {
       // Create a new div to hold the new content
       var newContent = $('<div>').attr('id', 'newContent').css({
         position: 'relative',
-        top: '8px',
+        top: '5px',
         right: '-100%',
         height: 'fill-content',
         background: '#fff'
