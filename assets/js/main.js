2fcc0d1840860912b3587de19124bc02d0ffdc86
sidrxo <sidrxo@gmail.com>
2023-03-06 14:25:59 +0000

    asdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,32 @@ function myFunction() {
 
 function loadProjectPage(url) {
   $.get(url, function(data) {
-    // Fade out the .doubletile and .doubletile2 elements
-    $('.doubletile, .doubletile2').fadeOut('slow', function() {
-      // Once the fade-out is complete, update the content of #projectContent
-      $('#projectContent').html(data);
-      // Fade in the #projectContent element
-      $('#projectContent').fadeIn('slow');
-      // Run /assets/js/clickbuttons.js after the new content has loaded
-      $.getScript('/assets/js/clickbuttons.js');
+    // Slide out the .doubletile and .doubletile2 elements to the left
+    $('.doubletile, .doubletile2').animate({left: '-100%'}, 'slow', function() {
+      // Create a new div to slide in from the right
+      var newContent = $('<div>').attr('id', 'newContent').css({
+        position: 'absolute',
+        top: 0,
+        right: '-100%',
+        width: '100%',
+        height: '100%',
+        background: '#fff'
+      }).appendTo('body');
+      // Populate the new div with the fetched data
+      newContent.html(data);
+      // Slide in the new div from the right
+      newContent.animate({right: 0}, 'slow', function() {
+        // Once the slide-in is complete, remove the old content and the new div
+        $('.doubletile, .doubletile2').remove();
+        $('#projectContent').removeAttr('style').empty().append(newContent.contents());
+        newContent.remove();
+        // Run /assets/js/clickbuttons.js after the new content has loaded
+        $.getScript('/assets/js/clickbuttons.js');
+      });
     });
   });
 }
 
+
   
+$.getScript('/assets/js/clickbuttons.js');
